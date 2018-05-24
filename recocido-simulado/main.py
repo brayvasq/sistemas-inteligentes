fcd3236e4967cbecdc7ece39e0cb184d0b304464
Jose Castro Arias <joseocastroarias@gmail.com>
2018-05-24 14:16:51 -0500

    recocido simulado

--- recocido-simulado/main.py
+++ recocido-simulado/main.py
@@ -2,37 +2,41 @@ import random
 import math
 
 def function(x, y):
-	return (1.5-x +(x*y))*(1.5-x +(x*y)) + (2.25-x+(x*y))*(2.25-x+(x*y)) + (2.625-x+(x*y))*(2.625-x+(x*y))
+	return ((1.5 - x + (x*y))**2) + ((2.25 - x + ((x*y)**2))**2) + ((2.625 - x + ((x*y)**3))**2)
+	# (1.5-x +(x*y))^2 + (2.25-x+(x*y))^2 + (2.625-x+(x*y))^2
+	#return (3 * ((x**2) + (y**2))) + (4*x*y) + (5*x) + (6*y) + 7
 
 def approximations(a0, b0, Tmax, Tmin, rateCold, alfa):
 	x = random.randrange(a0, b0)/10
 	y = random.randrange(a0, b0)/10
-	print("X, Y inicial: ", x, y)
+	print("x, y inicial: ", x, y)
 	T = Tmax
 	n=0
 	while T>Tmin:
 		n+=1
 		xNew = x+(alfa*random.gauss(0, 1))
 		yNew = y+(alfa*random.gauss(0, 1))
+		if (xNew<a0):
+			xNew=a0
+		elif (xNew>(b0/10)):
+			xNew=b0/10
+		if (yNew<a0):
+			yNew=a0
+		elif (yNew>(b0/10)):
+			yNew=b0/10
 		deltaE=function(xNew, yNew)-function(x, y)
 		if function(x, y)>function(xNew, yNew):
 			x=xNew
 			y=yNew
-		elif math.exp(deltaE/T)>random.randrange(0,1):
+		elif math.exp(-(abs(deltaE))/T)>random.randrange(0,10)/10:
 			x=xNew
 			y=yNew
 		T=T*rateCold
-		if (x<a0/10):
-			x=a0/10
-		elif (x>(b0/10)):
-			x=b0/10
-		if (y<a0/10):
-			y=a0/10
-		elif (y>(b0/10)):
-			y=b0/10
+		
 	print(x, y)
+	print("Evaluación", function(x, y))
 	print("Iteraciones: ", n)
-			
 
-approximations(-20, 20, 2, 0.001, 0.9, 0.01)
+approximations(-20, 20, 2, 0.0001, 0.999, 0.1)
+
 
